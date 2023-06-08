b3adaca6263372349e0ac3e4c30684c3ee72553d
gohyiqian <kenji.gohyiqian@gmail.com>
2023-06-08 22:14:48 +0800

    test basepath

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: "export", basePath: "dev" };
+const nextConfig = { output: "export", basePath: "/dev" };
 
 module.exports = nextConfig;
